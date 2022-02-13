4a0fe5d26748387289c187bdc4927296bfce9ce2
theabhayprajapati <theabhayprajapati@gmail.com>
2022-02-13 21:40:53 +0530

    Add profile page

---
+++ firebase.tsx
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDbwMQAa8e7GNPCN9aBE1gDAJs0_JG2qq4",
+    authDomain: "socialconnect-e4cdf.firebaseapp.com",
+    projectId: "socialconnect-e4cdf",
+    storageBucket: "socialconnect-e4cdf.appspot.com",
+    messagingSenderId: "572770083866",
+    appId: "1:572770083866:web:e14a3228a640efe8bb5da3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore()
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,92 @@
-import Head from 'next/head'
+import React from 'react'
 
-export default function Home() {
+const Index = () => {
   return (
-    <div >
-      <Head>
-        <title>
-          Abhay's Builds
-        </title>
-      </Head>
-
-      <button className='bg-white rounded-full p-1 px-3 hover:bg-slate-50 active:bg-slate-200 z-50 absolute top-5 '>
-        Login or Join
-      </button>
-
-      <main className=" bg-[#E8E9EF] font-Saira border-2 border-black m-3 h-screen flex-col md:flex">
-        <div className='grid place-items-center  m-5'>
-          <div className="border-2 border-black grid place-items-center">
-            <img src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1085&q=80" alt="" className='h-20 w-20 object-contain rounded-full' />
-            <h1 className="font-semibold">
-              Abhay Prajapati
-            </h1>
-            <h1>
-              @abhayprajapati
-            </h1>
-            <h1>
-              Next JS / Firebase
-            </h1>
-            <div className="flex justify-evenly gap-1">
-              <h1>Twitter</h1>
-              <h1>Github</h1>
-              <h1>LinkedIn</h1>
-              <h1>Instagram</h1>
-            </div>
-          </div>
-
+    <div className="w-screen text-2xl font-Saira text-gray-800  h-screen">
+      <div className="absolute top-5 left-5 pb-10">
+        <button className='bg-white px-5 py-2 text-sm sm:text-xl rounded-full shadow-lg hover:bg-gray-100 active:border-2 border-white hover:scale-95 transform transition duration-200 ease-out'>
+          Login or join
+        </button>
+      </div>
+      <div className="max-w-xl h-full mt-20 mx-auto flex flex-col items-center  p-10">
+        <div className="bg-red-400 rounded-full self-center h-32 w-32">
+          asdfkj
         </div>
-        <div className="border-2">
-          <div className="border-2 bg-red-400 w-[350px] h-[80px] rounded-lg mx-2 flex shadow-lg">
-            <div className=" w-[30%] bg-gray-500 h-full rounded-lg grid place-items-center">
-              LOGO
+        <h1 className="font-semibold">
+          Abhay Prajapati
+        </h1>
+        <p className='text-lg'>
+          @abhayprajapati
+        </p>
+        <h2 className=''>
+          Next JS/ Firebase
+        </h2>
+        <div className="flex items-center space-x-5">
+          <h1>Twitter</h1>
+          <h1>Instagram</h1>
+          <h1>Github</h1>
+          <h1>LinkedIn</h1>
+        </div>
+        <div className='grid grid-cols-1 w-full mt-10 gap-y-5'>
+          <div className='bg-white shadow-lg h-24 rounded-lg cursor-pointer hover:scale-105 transform transition duration-200 ease-out'>
+            <div>
+              {/* //todo: Image */}
             </div>
-            <div className="grid place-items-center w-[70%]">
-              <h1>
-                DISCORD
-              </h1>
-              <p className='text-center'>
-                Lorem ipsum dolor sit.
-              </p>
+            <h1>
+              skdf
+              {/* //todo: Portfolio name */}
+            </h1>
+          </div>
+          <div className='bg-white shadow-lg h-24 rounded-lg cursor-pointer hover:scale-105 transform transition duration-200 ease-out'>
+            <div>
+              {/* //todo: Image */}
             </div>
+            <h1>
+              skdf
+              {/* //todo: Portfolio name */}
+            </h1>
+          </div><div className='bg-white shadow-lg h-24 rounded-lg cursor-pointer hover:scale-105 transform transition duration-200 ease-out'>
+            <div>
+              {/* //todo: Image */}
+            </div>
+            <h1>
+              skdf
+              {/* //todo: Portfolio name */}
+            </h1>
+          </div><div className='bg-white shadow-lg h-24 rounded-lg cursor-pointer hover:scale-105 transform transition duration-200 ease-out'>
+            <div>
+              {/* //todo: Image */}
+            </div>
+            <h1>
+              skdf
+              {/* //todo: Portfolio name */}
+            </h1>
+          </div><div className='bg-white shadow-lg h-24 rounded-lg cursor-pointer hover:scale-105 transform transition duration-200 ease-out'>
+            <div>
+              {/* //todo: Image */}
+            </div>
+            <h1>
+              skdf
+              {/* //todo: Portfolio name */}
+            </h1>
+          </div><div className='bg-white shadow-lg h-24 rounded-lg cursor-pointer hover:scale-105 transform transition duration-200 ease-out'>
+            <div>
+              {/* //todo: Image */}
+            </div>
+            <h1>
+              skdf
+              {/* //todo: Portfolio name */}
+            </h1>
           </div>
         </div>
-      </main>
-
-
+        <footer className="m-32  w-full flex justify-around pb-10">
+          <h1>Join</h1>
+          <h1>Know</h1>
+          <h1>Connect</h1>
+        </footer>
+      </div>
     </div>
   )
 }
+
+export default Index
