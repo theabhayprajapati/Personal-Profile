3c1986aef5ca420d726a492a94831d9dc966e057
theabhayprajapati <theabhayprajapati@gmail.com>
2022-02-14 19:48:54 +0530

    added more details

---
+++ pages/join.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Join = () => {
+    return (
+        <div className='max-w-6xl mx-auto mt-10 font-Saira px-5'>
+
+            <h1 className='text-3xl font-medium mt-5 bg-blue-100 px-2 rounded-lg'>
+                The Only Link You’ll Ever Need to Showcase Your Projects
+            </h1>
+            <h1 className="text-4xl font-semibold mt-5 ">
+                Showcase
+                <p className='font-medium text text-3xl mt-2'>
+                    is place where you can put your projects and showcase your skills,
+                    a simple <span className="text-blue-500">link</span> for all your projects.
+                </p>
+            </h1>
+        </div>
+    )
+}
+
+export default Join
